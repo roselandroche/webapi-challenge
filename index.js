bb66efcd82eeef0e8bae3b0cf9d293fc67b3fb48
roselandroche <r.landroche@gmail.com>
2019-12-13 20:23:25 -0500

    CRUD complete for projects

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const projectsRouter = require("./routers/projects")
 
 const server = express()
 
 server.use(express.json())
+server.use("/projects", projectsRouter)
 
 server.listen(3000, () => {
     console.log(`\n*** Server listening on Port: 3000 ***\n`)
--- routers/projects.js
+++ routers/projects.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const Projects = ("../data/helpers/projectModel.js")
+const projects = require("../data/helpers/projectModel.js")
 
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    Projects.get()
+    projects.get()
         .then(data => {
             res.json(data)
         })
@@ -13,14 +13,45 @@ router.get("/", (req, res) => {
             res.status(500).json(error)
         })
 })
-// async function getAllProjects(req, res) {
-//     try {
-//         const projects = await Projects.get();
-//         res.json(projects);
-//     } 
-//     catch (error) {
-//         res.status(500).json(error)
-//     }
-// }
+
+router.get("/:id", (req, res) => {
+    projects.get(req.params.id)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+router.post("/", (req, res) => {
+    projects.insert(req.body)
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+router.put("/:id", (req, res) => {
+    projects.update(req.params.id, req.body)
+        .then(updatedProject => {
+            res.json(updatedProject)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+router.delete("/:id", (req, res) => {
+    projects.remove(req.params.id)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
 
 module.exports = router
