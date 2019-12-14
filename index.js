e35e7a6a91c574587e8cb16649b4fa324f708f74
roselandroche <r.landroche@gmail.com>
2019-12-13 20:30:11 -0500

    Working on actions router

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express")
+const morgan = require("morgan")
+
 const projectsRouter = require("./routers/projects")
+const actionsRouter = require("./routers/actions")
 
 const server = express()
 
 server.use(express.json())
 server.use("/projects", projectsRouter)
+server.use("/actions", actionsRouter)
 
 server.listen(3000, () => {
     console.log(`\n*** Server listening on Port: 3000 ***\n`)
