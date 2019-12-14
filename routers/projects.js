caa5a41edaf674f97644e7dc40c3b66fa77b214c
roselandroche <r.landroche@gmail.com>
2019-12-13 19:42:42 -0500

    Working on route

--- routers/projects.js
+++ routers/projects.js
@@ -4,18 +4,23 @@ const Projects = ("../data/helpers/projectModel.js")
 
 const router = express.Router()
 
-// router.get("/", (req, res) => {
-//     projects.get(req.params.id)
-
-// })
-async function getAllProjects(req, res) {
-    try {
-        const projects = await Projects.get();
-        res.json(projects);
-    } 
-    catch (error) {
-        res.status(500).json(error)
-    }
-}
+router.get("/", (req, res) => {
+    Projects.get()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+// async function getAllProjects(req, res) {
+//     try {
+//         const projects = await Projects.get();
+//         res.json(projects);
+//     } 
+//     catch (error) {
+//         res.status(500).json(error)
+//     }
+// }
 
 module.exports = router
